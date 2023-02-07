5d34ac978009fd4e7777056e317b597a4889de31
JoeDGit <joe.dobbins89@gmail.com>
2023-02-07 12:52:13 +0000

    fix: awaits userlogout then push to landing page

--- components/LoggedInNav.jsx
+++ components/LoggedInNav.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
-import styles from "../css/loggedInNav.module.css";
-import Image from "next/image";
-import logo from "../images/Logo.svg";
-import Link from "next/link";
-import defaultAvatar from "../images/default-avatar.svg";
-import { TbMessage2 } from "react-icons/tb";
-import { useLogout } from "../hooks/useLogout";
-import { useRouter } from "next/router";
-import { useAuthContext } from "../hooks/useAuthContext";
-import Button from "./Button";
-import { GiHamburgerMenu } from "react-icons/gi";
-import Modal from "./Modal";
+import React, { useState } from 'react';
+import styles from '../css/loggedInNav.module.css';
+import Image from 'next/image';
+import logo from '../images/Logo.svg';
+import Link from 'next/link';
+import defaultAvatar from '../images/default-avatar.svg';
+import { TbMessage2 } from 'react-icons/tb';
+import { useLogout } from '../hooks/useLogout';
+import { useRouter } from 'next/router';
+import { useAuthContext } from '../hooks/useAuthContext';
+import Button from './Button';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import Modal from './Modal';
 
 export default function LoggedInNav() {
   //calls logout hook to logout
   const router = useRouter();
   const { logout } = useLogout();
   const { user } = useAuthContext();
-  
+
   const [showModal, setShowModal] = useState(false);
 
   const modal = (
@@ -33,12 +33,16 @@ export default function LoggedInNav() {
           }}
         />
       ) : (
-        <Image src={defaultAvatar} alt="profile" className={styles.avatarImage} />
+        <Image
+          src={defaultAvatar}
+          alt="profile"
+          className={styles.avatarImage}
+        />
       )}
-     
+
       <div>Logged in {user.displayName}</div>
       <Link href="/conversations" className={styles.message_container}>
-        <TbMessage2 size={30} className={styles.messages} />{" "}
+        <TbMessage2 size={30} className={styles.messages} />{' '}
         <span className={styles.messages}>Messages</span>
       </Link>
       <Button
@@ -53,13 +57,14 @@ export default function LoggedInNav() {
 
   const handleLogout = (e) => {
     e.preventDefault();
-    logout();
-    router.push("/");
+    logout().then(() => {
+      router.push('/');
+    });
   };
   return (
     <nav>
       <div className={styles.navContainer}>
-        <Link href={"/home"}>
+        <Link href={'/home'}>
           <Image className={styles.logoStyle} alt="logo" src={logo} />
         </Link>
         {modal}
@@ -71,21 +76,25 @@ export default function LoggedInNav() {
         />
         <div className={styles.buttonContainerStyle}>
           <div className={styles.userInfoStyles}>
-           
             {user.photoURL ? (
-               /* eslint-disable-next-line @next/next/no-img-element */
-            <img
-              src={user.photoURL}
-              alt="profile "
-              className={styles.avatarImage}
-              onClick={() => {
-                router.push(`/users/${user?.displayName}`);
-              }}
-            /> ) : (
-              <Image src={defaultAvatar} alt="profile" className={styles.avatarImage} />
+              /* eslint-disable-next-line @next/next/no-img-element */
+              <img
+                src={user.photoURL}
+                alt="profile "
+                className={styles.avatarImage}
+                onClick={() => {
+                  router.push(`/users/${user?.displayName}`);
+                }}
+              />
+            ) : (
+              <Image
+                src={defaultAvatar}
+                alt="profile"
+                className={styles.avatarImage}
+              />
             )}
-            <Link href={"/conversations"}>
-              <TbMessage2 size={30} style={{ color: "white" }} />{" "}
+            <Link href={'/conversations'}>
+              <TbMessage2 size={30} style={{ color: 'white' }} />{' '}
             </Link>
             <div className={styles.username}>{user.displayName}</div>
           </div>
