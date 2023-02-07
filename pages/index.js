cd4612546ce05ad183405ec5f60fa04203085e0b
JoeDGit <joe.dobbins89@gmail.com>
2023-02-07 12:17:21 +0000

    fix: add landing page redirect if user is loggedin

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-
-import Hero from "../components/landingPage/Hero";
-import Info from "../components/landingPage/Info";
-import SectionOne from "../components/landingPage/SectionOne";
-import SectionTwo from "../components/landingPage/SectionTwo";
+import Hero from '../components/landingPage/Hero';
+import Info from '../components/landingPage/Info';
+import SectionOne from '../components/landingPage/SectionOne';
+import SectionTwo from '../components/landingPage/SectionTwo';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  if (user) router.push('/home');
+
   return (
-   <div>
-    <Hero/>
-    <SectionOne/>
-    <Info/>
-    <SectionTwo/>
-   </div>
+    <div>
+      <Hero />
+      <SectionOne />
+      <Info />
+      <SectionTwo />
+    </div>
   );
 }
-
