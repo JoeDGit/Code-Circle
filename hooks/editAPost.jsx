67be7be478f7a957b983433f849e627651e87501
JoeDGit <joe.dobbins89@gmail.com>
2023-04-05 16:22:35 +0100

    refactors edit a post to use availability

--- hooks/editAPost.jsx
+++ hooks/editAPost.jsx
@@ -1,12 +1,20 @@
 import { db } from '../firebase/config';
 import { doc, updateDoc } from 'firebase/firestore';
 
-export default async function editAPost(postId, newProjectDescription, newProgrammingLanguage, newTimeToCode, newTimeZone) {
-    const newPostInfo = {
-        projectDescription: newProjectDescription,
-        programmingLanguage: newProgrammingLanguage,
-        timeToCode: newTimeToCode,
-        timeZone: newTimeZone
-    };
-    await updateDoc(doc(db, "posts", postId), newPostInfo)
+export default async function editAPost(
+  postId,
+  newProjectDescription,
+  newProgrammingLanguage,
+  newWeekDayAvailability,
+  newDailyAvailability,
+  newTimeZone
+) {
+  const newPostInfo = {
+    projectDescription: newProjectDescription,
+    programmingLanguage: newProgrammingLanguage,
+    weekDayAvailability: newWeekDayAvailability,
+    dailyAvailability: newDailyAvailability,
+    timeZone: newTimeZone,
+  };
+  await updateDoc(doc(db, 'posts', postId), newPostInfo);
 }
