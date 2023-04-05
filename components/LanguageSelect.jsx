c16300f79e4376b5f0dba2409ee158e034335bdd
JoeDGit <joe.dobbins89@gmail.com>
2023-04-05 13:22:55 +0100

    adds customStyle feature to languageSelect comp

--- components/LanguageSelect.jsx
+++ components/LanguageSelect.jsx
@@ -6,13 +6,14 @@ import React from 'react';
 export default function LanguageSelect({
   selectedLanguages,
   setSelectedLanguages,
+  customStyle,
 }) {
   const handleChange = (selectedOptions) => {
     setSelectedLanguages(selectedOptions.map((option) => option.value));
   };
 
   return (
-    <div className="mt-4 mb-4">
+    <div className={`mt-4 mb-4 ${customStyle}`}>
       <Select
         id="language-select"
         isMulti
