ad7cbb7c845ae98a75eed5b18981f7ad5478a090
JoeDGit <joe.dobbins89@gmail.com>
2023-04-07 22:17:58 +0100

    refactor usernamefromparams in parent component

--- pages/users/[displayname].js
+++ pages/users/[displayname].js
@@ -1,20 +1,26 @@
-import React, { useEffect, useState } from "react";
-import styles from "../../css/userProfile.module.css";
-import { useRouter } from "next/router";
-import UserProfile from "../../components/UserProfile";
-import { useAuthContext } from "../../hooks/useAuthContext";
-import PastPosts from "../../components/PastPosts";
-import checkLoggedIn from "../../hooks/checkLoggedIn";
+import React, { useEffect, useState } from 'react';
+import styles from '../../css/userProfile.module.css';
+import { useRouter } from 'next/router';
+import UserProfile from '../../components/UserProfile';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import PastPosts from '../../components/PastPosts';
+import checkLoggedIn from '../../hooks/checkLoggedIn';
 
 export default function OtherUserProfile() {
   checkLoggedIn();
   const { user } = useAuthContext();
+  const router = useRouter();
+  const userNameFromParams = router.query.displayname;
 
   return (
     <div className={styles.pageContainer}>
-      <h1 className={styles.heading}>User Profile</h1>
-      <UserProfile userName={user?.displayName} photoURL={user?.photoURL} />
-      <PastPosts userName={user?.displayName}/>
+      <h1 className={styles.heading}>{userNameFromParams}&apos;s Profile</h1>
+      <UserProfile
+        userName={user?.displayName}
+        userNameFromParams={userNameFromParams}
+        photoURL={user?.photoURL}
+      />
+      <PastPosts userName={user?.displayName} />
     </div>
   );
 }
