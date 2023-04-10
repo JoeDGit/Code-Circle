0a669b7bf4bbfa4569df1ba658c4bf5aa87a9a88
JoeDGit <joe.dobbins89@gmail.com>
2023-04-10 02:09:11 +0100

    config changes

--- firebase/config.js
+++ firebase/config.js
@@ -5,8 +5,8 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: String(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
-  authDomain: String(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
-  projectId: String(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: 'code-circle-e4a7a.appspot.com',
   messagingSenderId: '607448944550',
   appId: '1:607448944550:web:d2ec26786b015639c60967',
