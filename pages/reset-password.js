bad225035bce9760fbca048965d3cf127f705d5e
JoeDGit <joe.dobbins89@gmail.com>
2023-04-10 02:00:00 +0100

    minor change to trigger redeploy

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -7,7 +7,7 @@ export default function ResetPassword() {
     <div className="flex">
       <FormSide
         title="Forgot Password?!"
-        paragraph1="Forgot your password? No problem. Just enter your email address and we'll send you instructions on how to reset it. If you're having trouble accessing your account, please don't hesitate to contact our support team for assistance. We're here to help you get back to coding as quickly and smoothly as possible."
+        paragraph1="Forgot your password? No problem. Just enter your email and we'll send you instructions on how to reset it. If you're having trouble accessing your account, please don't hesitate to contact our support team for assistance. We're here to help you get back to coding as quickly and smoothly as possible."
       />
       <ResetPasswordForm />
     </div>
