ef2a28806d26544bf7ee53c7467ffbc621351118
JoeDGit <joe.dobbins89@gmail.com>
2023-04-11 02:53:28 +0100

    refactors getReplies to return all replies when no postId is passed in

--- hooks/getReplies.jsx
+++ hooks/getReplies.jsx
@@ -4,7 +4,10 @@ import { db } from '../firebase/config';
 
 export async function getReplies(postId) {
   const repliesCollection = collection(db, 'replies');
-  const q = query(repliesCollection, where('postId', '==', postId));
+  let q = query(repliesCollection);
+  if (postId) {
+    q = query(repliesCollection, where('postId', '==', postId));
+  }
   const repliesSnapshot = await getDocs(q);
   const postReplies = repliesSnapshot.docs.map((doc) => doc.data());
   return postReplies;
